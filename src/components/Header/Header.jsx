fbef9ee81b4d9696c38d6cb114fdd9f9b3cb2fb4
jerioz <escobedoranero@gmail.com>
2023-06-26 10:54:25 +0200

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,9 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useContext } from "react";
 import { UserContext } from '../../context/UserContext/UserState'
 
+import { Menu } from 'antd'
+import { LogoutOutlined, LoginOutlined, UserOutlined, HomeOutlined, UserAddOutlined } from '@ant-design/icons'
+
 const Header = () => {
   const { token, logout } = useContext(UserContext)
 	const navigate = useNavigate()
@@ -21,7 +24,7 @@ const Header = () => {
     <div>
     <header className= 'header__newsPage'>
         <h1 className='header__newsPage-title'>Blue Suede Shoes Records</h1>
-        <nav className='menu-nav'>
+        {/* <nav className='menu-nav'>
             <ul className='menu-nav__newsPage-list'>
                 <li className='menu-nav__newsPage-item'><Link to='/' className='menu-nav__newsPage-link'>Home</Link></li>
                 <li className='menu-nav__newsPage-item'><Link to='/register' className='menu-nav__newsPage-link'>Register</Link></li>
@@ -40,7 +43,35 @@ const Header = () => {
                 </span>
                  )}
             </ul>
-        </nav>
+        </nav> */}
+        <nav>
+			<Menu mode="horizontal" className='menu-nav__menu' defaultSelectedKeys={['profile']}>
+      <Menu.Item key="home" icon={<HomeOutlined />}>
+						<Link to="/">Home</Link>
+					</Menu.Item>
+          <Menu.Item key="register" icon={<UserAddOutlined />}>
+						<Link to="/register">Register</Link>
+					</Menu.Item>
+				{token ? (
+					<>
+						<Menu.Item
+							key="logout"
+							icon={<LogoutOutlined />}
+							onClick={logoutUser}
+						>
+							<Link to="/logout">Logout</Link>
+						</Menu.Item>
+						<Menu.Item key="profile" icon={<UserOutlined />}>
+							<Link to="/profile">Profile</Link>
+						</Menu.Item>
+					</>
+				) : (
+					<Menu.Item key="login" icon={<LoginOutlined />}>
+						<Link to="/login">Login</Link>
+					</Menu.Item>
+				)}
+			</Menu>
+		</nav>
     </header>
     </div>
     </React.Fragment>
