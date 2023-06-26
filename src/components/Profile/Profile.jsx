226048f09ccbe50d78a9e1cf1fe1f3bf7802cc4f
jerioz <escobedoranero@gmail.com>
2023-06-26 16:10:11 +0200

    update profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect} from "react";
 import { UserContext } from '../../context/UserContext/UserState'
 import './Profile.styles.scss'
+import { Card, Space } from 'antd';
 
 const Profile = () => {
     const { getUserInfo, user } = useContext(UserContext);
@@ -17,10 +18,20 @@ const Profile = () => {
     
     return (
     <>
-    <div className="data">
+    <div className="profile__data">
         <h1>Profile</h1>
-        <h3>Name: {user.name}</h3>
-        <h3>Email: {user.email}</h3>
+        {/* <h3>Name: {user.name}</h3>
+        <h3>Email: {user.email}</h3> */}
+        <Space direction="vertical" size={16}>
+    <Card className="profile__card"
+      title={user.name}
+      style={{
+        width: 300,
+      }}
+    >
+      <p>{user.email}</p>
+    </Card>
+  </Space>
     </div>
     </>
     )
